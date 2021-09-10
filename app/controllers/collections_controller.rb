81dc000dd22a754d7f7bdf8327f948bdbc45dd1f
byarenny <byarenny@gmail.com>
2021-09-10 14:03:42 -0700

    adds controller methods for show and create as well as tests

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,4 +5,16 @@ class CollectionsController < ApplicationController
     collections = current_user.collections
     render json: collections
   end
+  def show 
+    collection = Collection.find(params[:id])
+    render json: collection
+  end
+  def create
+    collection = Collection.create(collection_params)
+    render json: collection
+  end
+  private 
+  def collection_params
+    params.require(:collection).permit(:name, :description, :priority)
+  end
 end
--- spec/requests/collections_spec.rb
+++ spec/requests/collections_spec.rb
@@ -12,6 +12,7 @@ RSpec.describe "Collections", type: :request do
         password_confirmation:"123456"
       }
     }
+
     it "gets nothing if user is not signed in" do 
 
       user = User.create(user_attributes)
@@ -72,5 +73,68 @@ RSpec.describe "Collections", type: :request do
 
     
     
+  end
+  describe "GET /show" do
+  
+    let (:user_attributes){
+      {
+        user_name: "test user",
+        email: "test.user@mail.com",
+        password:"123456",
+        password_confirmation:"123456"
+      }
+    }
+    let (:collection_attributes){
+      {
+        name: "Saturday Spots",
+        description: "My favorite spots to surf on Saturday",
+        priority: "3"
+      }
+    }
+    it "gets nothing if user is not signed in" do
+    user = User.create(user_attributes)
+    collection = user.collections.create(collection_attributes)
+    
+
+    get "/collections/#{collection.id}"
+    #expect 302
+
+    expect(response).to have_http_status(302)
+    end
+    it "Shows one collection of user" do
+      user = User.create(user_attributes)
+      collection = user.collections.create(collection_attributes)
+      
+      sign_in user
+
+      get "/collections/#{collection.id}"
+      #expect 200
+      
+      expect(response).to have_http_status(200)
+      expect(collection.name).to include "Saturday Spots"
+      end
+  end
+  describe "POST /create" do
+    let (:user_attributes){
+      {
+        user_name: "test user",
+        email: "test.user@mail.com",
+        password:"123456",
+        password_confirmation:"123456"
+      }
+    }
+    it "Creates a new collection" do 
+      collection_params = {
+      collection:{
+        name: "Saturday Spots",
+        description: "My favorite spots to surf on Saturday",
+        priority: "3"
+      }
+    }
+    user = User.create(user_attributes)
+    sign_in user
+    post "/collections", params: collection_params
+    expect(response).to have_http_status(200)
+    end
   end
 end
