cf08ab2b64a9002023efd388cd1895d4792b42fa
celoazevedo <meazevedo86@gmail.com>
2021-09-13 09:58:53 -0700

    adds request spec test for spots.

--- spec/requests/spots_spec.rb
+++ spec/requests/spots_spec.rb
@@ -2,6 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Spots", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "makes a get request to /spots" do
+      spot_attributes = { 
+        name: 'San Miguel', 
+        description: 'Surf spot for San Miguel', 
+        easy_parking: true, 
+        bottom: 'rock', 
+        latitude: 32.088172, 
+        longitude: -116.884867, 
+        wind_direction: 35, 
+        swell_direction: 290, 
+        surf_height_min: 3.0, 
+        surf_height_max: 8.0,
+        tide: 'high'
+    }
+
+    s = Spot.create(spot_attributes)
+    get "/spots"
+    spot_response = JSON.parse(response.body)
+    expect(response).to have_http_status(200)
+    expect(spot_response.length).to eq 1
+    end
+
+
   end
 end
