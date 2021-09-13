ec42846660ebde4c09e3dbf38e150c6a6f20bb09
byarenny <byarenny@gmail.com>
2021-09-13 14:17:53 -0700

    adds redirect

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -18,85 +18,100 @@ import {
 } from "react-router-dom"
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-      this.state = {
-        collections: [],
-        surfSpots: [] 
-      }
-    
+    this.state = {
+      collections: [],
+      surfSpots: []
+    }
+
   }
 
   // was getting an unexpected token error.
-  componentDidMount(){
+  componentDidMount() {
     this.props.logged_in ? this.readCollections() : console.log(this.state.collections)
     this.readSpots()
   }
 
   // should we have a catch block for potential errors?
-  readCollections = async () =>{ 
+  readCollections = async () => {
     const response = await fetch("/collections")
     const result = await response.json()
-    this.setState({collections: result})
+    this.setState({ collections: result })
   }
 
   // read Spots
   readSpots = async () => {
     const response = await fetch("/spots")
     const result = await response.json()
-    this.setState({surfSpots: result})
+    this.setState({ surfSpots: result })
   }
-  
+
+  // creating a new collection
+  createCollection = async (newCollection) => {
+    newCollection.user_id = this.props.current_user.id
+    const response = await fetch("/collections", {
+      body: JSON.stringify(newCollection),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST"
+    })
+    await response.json()
+    this.readCollections()
+  }
+
   render() {
-    console.log(this.state.collections)
-    console.log(this.state.surfSpots)
-    const { 
-      logged_in, 
-      new_user_route, 
-      sign_in_route, 
-      sign_out_route, 
-      current_user 
+    // console.log(this.state.collections)
+    // console.log(this.state.surfSpots)
+    const {
+      logged_in,
+      new_user_route,
+      sign_in_route,
+      sign_out_route,
+      current_user
     } = this.props
     return (
       <Router>
-        <Header 
-          logged_in={logged_in} 
-          new_user_route={new_user_route} 
-          sign_in_route={sign_in_route} 
-          sign_out_route={sign_out_route} 
+        <Header
+          logged_in={logged_in}
+          new_user_route={new_user_route}
+          sign_in_route={sign_in_route}
+          sign_out_route={sign_out_route}
           current_user={current_user}
         />
         <Switch>
           <Route exact path="/" component={Home} />
-          
+
           {logged_in &&
-          <Route 
-            path="/surfspotindex" 
-            render={() => 
-            < SurfSpotIndex surfSpots={this.state.surfSpots}/>}
-          />}
+            <Route
+              path="/surfspotindex"
+              render={() =>
+                < SurfSpotIndex surfSpots={this.state.surfSpots} />}
+            />}
 
           <Route path="/surfspotshow/:id" component={SurfSpotShow} />
-          <Route path="/collectionnew" component={CollectionNew} />
+          <Route path="/collectionnew" render={() => <CollectionNew user_id={current_user.id} createCollection={this.createCollection}/>
+          } />
           <Route path="/collectionedit" component={CollectionEdit} />
           <Route path="/aboutus" component={AboutUs} />
 
           {/* using filter here to get the collections that bellongs to a specific user */}
-          {logged_in && 
-            <Route 
-              path="/mycollectionsindex" 
+          {logged_in &&
+            <Route
+              path="/mycollectionsindex"
               render={() => {
                 let user_id = current_user.id
                 let collections = this.state.collections.filter(collection => collection.user_id === +user_id)
-              return < MyCollectionsIndex user_id={user_id} collections={collections}/>
-          }}/>}
+                return < MyCollectionsIndex user_id={user_id} collections={collections} />
+              }} />}
 
-          <Route 
-            path="/mycollectionsshow/:id" 
+          <Route
+            path="/mycollectionsshow/:id"
             render={(props) => {
               let id = props.match.params.id
               let collection = this.state.collections.find(collection => collection.id === +id)
-            return < MyCollectionsShow collection={collection}/>
+              return < MyCollectionsShow collection={collection} />
             }}
           />
           <Route component={NotFound} />
