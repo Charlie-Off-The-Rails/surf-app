d24cc8f7bf60b36ba578b1495aa33363a54cc702
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-09-15 14:33:32 -0700

    added endpoint for collection_spot creation

--- app/models/collection_spot.rb
+++ app/models/collection_spot.rb
@@ -1,4 +1,5 @@
 class CollectionSpot < ApplicationRecord
     belongs_to :spot
     belongs_to :collection
+    validates :collection, uniqueness: { scope: :spot, message: "each collection_spot pair should be unique." }
 end
