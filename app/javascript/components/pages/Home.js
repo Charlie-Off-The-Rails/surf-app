ed01120b78484ac075fb1d1f3e03aca82416eb31
byarenny <byarenny@gmail.com>
2021-09-20 19:17:13 -0700

    adds padding to home page

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import homepage from "../assets/homepage.png";
+import StyledPageTitle from "../components/StyledPageTitle";
+
 
 class Home extends Component {
   render() {
     return (
       <>
-        <div>
-          <img
-            className="object-cover w-full max-w-full h-full max-h-full"
+        <StyledPageTitle>Welcome to Cowabunga 411!</StyledPageTitle>
+        <div className= "px-8">
+            <img
+            className="object-cover w-full max-w-full h-full max-h-full rounded-lg shadow-lg"
             src={homepage}
             alt="Welcome homepage"
-          />
+            />
         </div>
       </>
     );
