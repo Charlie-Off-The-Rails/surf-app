35ddb930e399452dac871867029977adfc0030aa
ShrikeFound <alfonso.martinez@shrikefound.com>
2021-09-21 12:36:38 -0700

    added rows to index, max width to show for surfspots and collections. added spot links to collection cards

--- app/javascript/components/components/CardFormat.js
+++ app/javascript/components/components/CardFormat.js
@@ -3,10 +3,10 @@ import React, { Component } from "react";
 class CardFormat extends Component {
   render() {
     return (
-      <div className="grid grid-rows-3 grid-flow-col gap-x-4 gap-y-10 pb-10">
+      <div className="grid md:grid-cols-2 gap-x-4 gap-y-10 w-11/12 max-w-screen-xl mx-auto">
         {this.props.children}
       </div>
-    );
+    )
   }
 }
 
--- app/javascript/components/pages/SurfSpotShow.js
+++ app/javascript/components/pages/SurfSpotShow.js
@@ -11,35 +11,55 @@ class SurfSpotShow extends Component {
     const { surfSpot, spotData } = this.props;
     return (
       <>
-        <div className="pt-10">
-          <CardFormat>
-            <CardStyle>
-              <StyledCardTitle>{surfSpot.name}</StyledCardTitle>
-                <StyledCardDetails>
-                  <p className="mx-auto">{surfSpot.description}</p>
-                  <p className="mx-auto">{`Bottom: ${surfSpot.bottom}`}</p>
-                  <p className="mx-auto">{`Tide: ${surfSpot.tide}`}</p>
-                  <p className="mx-auto">{`Ideal Swell Direction(s): ${surfSpot.swell_direction} `}</p>
-                  <p className="mx-auto">{`Ideal Wind Direction(s): ${surfSpot.wind_direction}`}</p>
+        <div className="pt-10 w-11/12 max-w-screen-md mx-auto">
+          <CardStyle>
+            <StyledCardTitle>{surfSpot.name}</StyledCardTitle>
+            <StyledCardDetails>
+              <p className="mx-auto">{surfSpot.description}</p>
+              <p className="mx-auto">{`Bottom: ${surfSpot.bottom}`}</p>
+              <p className="mx-auto">{`Tide: ${surfSpot.tide}`}</p>
+              <p className="mx-auto">{`Ideal Swell Direction(s): ${surfSpot.swell_direction} `}</p>
+              <p className="mx-auto">{`Ideal Wind Direction(s): ${surfSpot.wind_direction}`}</p>
               {spotData && (
                 <>
-                  <p className={surfSpot.swell_direction.includes(spotData.swellDir) ? "text-green-400 mx-auto" : "mx-auto"}>Current Swell Direction: {spotData.swellDir}</p>
-                  <p className={surfSpot.wind_direction.includes(spotData.windDir) ? "text-green-400 mx-auto" : "mx-auto"}>Current Wind Direction: {spotData.windDir}</p>
-                  <p className="mx-auto">Current Swell Height: {spotData.swellHeight} ft</p>
-                  <p className="mx-auto">Current Surf Height: {spotData.surfHeight} ft</p>
-                  <p className="mx-auto">Current Water Temperature: {spotData.waterTemp}°F</p>
-                </>                
+                  <p
+                    className={
+                      surfSpot.swell_direction.includes(spotData.swellDir)
+                        ? "text-green-400 mx-auto"
+                        : "mx-auto"
+                    }
+                  >
+                    Current Swell Direction: {spotData.swellDir}
+                  </p>
+                  <p
+                    className={
+                      surfSpot.wind_direction.includes(spotData.windDir)
+                        ? "text-green-400 mx-auto"
+                        : "mx-auto"
+                    }
+                  >
+                    Current Wind Direction: {spotData.windDir}
+                  </p>
+                  <p className="mx-auto">
+                    Current Swell Height: {spotData.swellHeight} ft
+                  </p>
+                  <p className="mx-auto">
+                    Current Surf Height: {spotData.surfHeight} ft
+                  </p>
+                  <p className="mx-auto">
+                    Current Water Temperature: {spotData.waterTemp}°F
+                  </p>
+                </>
               )}
-                </StyledCardDetails>
+            </StyledCardDetails>
 
-              <Link to="/surfspotindex">
-                <StyledButton>Back</StyledButton>
-              </Link>
-            </CardStyle>
-          </CardFormat>
+            <Link to="/surfspotindex">
+              <StyledButton>Back</StyledButton>
+            </Link>
+          </CardStyle>
         </div>
       </>
-    );
+    )
   }
 }
 
