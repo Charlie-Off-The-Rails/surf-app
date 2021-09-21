696042ddd62d76226abb21ca45abf561172f4b0d
byarenny <byarenny@gmail.com>
2021-09-20 21:44:22 -0700

    Centers text in footer and adds google font to page titles

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer className="bg-gray-light text-center h-8 w-full flex justify-around flex-col">
-        <p className="text-brown py-1 text-4x1 font-semibold transition-colors">
+      <footer className="bg-gray-light h-8 w-full flex justify-around flex-col">
+        <p className="text-center text-brown py-1 text-4x1 font-semibold transition-colors self-center">
           Team-Off-The-Rails
         </p>
       </footer>
